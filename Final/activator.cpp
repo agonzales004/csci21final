e116893b7db25f959fcff6857a72e4c429230eb0
agonzales004 <agonzales3890@gmail.com>
2016-05-25 22:14:01 +0000

    update

--- Final/activator.cpp
+++ Final/activator.cpp
@@ -9,34 +9,7 @@
 
 unsigned int Activator::getPrime(unsigned int prime) 
 {
-   unsigned int i = 3, count, c;
-    for(count = 2; count <= prime;)
-    {
-        for(c=2; c<=i -1; c++)
-        {
-            if(i%c == 0)
-            break;
-        }
-    }
-    if(c==i)
-    {
-        count++;
-    }
-    
-    return i;
-    
-    if(prime == 12345)
-    {
-        return 132241;
-    } 
-    else if(prime == 8719)
-    {
-        return 90023;
-    } 
-    else
-    {
-        return 109;
-    }
+   
 }
 
 unsigned int Activator::sumTheDigits(unsigned int value) 
